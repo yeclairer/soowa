1384c2091c831d814a44380ee70fac9ef320fbd4
yeclairer <dbsrud821@naver.com>
2021-11-15 12:41:18 +0900

    211115-4

--- soowa/soowa_web/static/webcam.js
+++ soowa/soowa_web/static/webcam.js
@@ -5,45 +5,26 @@
   context = canvas.getContext('2d'),
   video = document.getElementById('video'),
   vendorUrl = window.URL || window.webkitURL;
+  
+  var constraints = { audio: false, video: true };
 
-  if (navigator.mediaDevices === undefined) {
-    navigator.mediaDevices = {};
-  }
-  if (navigator.mediaDevices.getUserMedia === undefined) {
-    navigator.mediaDevices.getUserMedia = function(constraints) {
-  
-      // First get ahold of the legacy getUserMedia, if present
-      var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-  
-      // Some browsers just don't implement it - return a rejected promise with an error
-      // to keep a consistent interface
-      if (!getUserMedia) {
-        return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
-      }
-  
-      // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
-      return new Promise(function(resolve, reject) {
-        getUserMedia.call(navigator, constraints, resolve, reject);
+  navigator.mediaDevices.getUserMedia(constraints)
+      .then(function(stream) {
+          var video = document.querySelector('video');
+          // Older browsers may not have srcObject
+          if ("srcObject" in video) {
+              video.srcObject = stream;
+          } else {
+              // Avoid using this in new browsers, as it is going away.
+              video.src = window.URL.createObjectURL(stream);
+          }
+          video.onloadedmetadata = function(e) {
+              video.play();
+          };
+      })
+      .catch(function(err) {
+          console.log(err.name + ": " + err.message);
       });
-    }
-  }
-  navigator.mediaDevices.getUserMedia({ audio: true, video: true })
-.then(function(stream) {
-  var video = document.querySelector('video');
-  // Older browsers may not have srcObject
-  if ("srcObject" in video) {
-    video.srcObject = stream;
-  } else {
-    // Avoid using this in new browsers, as it is going away.
-    video.src = window.URL.createObjectURL(stream);
-  }
-  video.onloadedmetadata = function(e) {
-    video.play();
-  };
-})
-.catch(function(err) {
-  console.log(err.name + ": " + err.message);
-});
 
 /* version2
   navigator.getWebcam = (navigator.getUserMedia 
